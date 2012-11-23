5a3e04aba0bf0d7659bcab26eafc8278d9d0d055
Julien Kirsch <informatic.revolution@gmail.com>
2012-11-23 16:48:38 -0500

    First commit

--- Resources/public/js/jquery.fileupload-ui.js
+++ Resources/public/js/jquery.fileupload-ui.js
@@ -22,18 +22,76 @@
             autoUpload: true,
             dataType: 'json',
 
-            // The add callback is invoked as soon as files are added to the fileupload
-            // widget (via file input selection, drag & drop or add API call).
-            // See the basic file upload widget for more information:
-            add: function (e, data) {
-                //alert('add');
-                data.submit();
-            },
-            done: function (e, data) {
-                //alert('done');
-                //alert(data.files.length);
+            // Callback for global upload progress events:
+            progressall: function (e, data) {
+                var $this = $(this),
+                    progress = parseInt(data.loaded / data.total * 100, 10);
+                    
+                $('.progress .bar').css(
+                    'width',
+                    progress + '%'
+                );
+                
+                $('#test').html(
+                    $this.data('fileupload')._renderExtendedProgress(data)
+                );
+            }         
+        },
+        
+        _formatFileSize: function (bytes) {
+            if (typeof bytes !== 'number') {
+                return '';
             }
-        }
+            if (bytes >= 1000000000) {
+                return (bytes / 1000000000).toFixed(2) + ' GB';
+            }
+            if (bytes >= 1000000) {
+                return (bytes / 1000000).toFixed(2) + ' MB';
+            }
+            return (bytes / 1000).toFixed(2) + ' KB';
+        },        
+        
+        _formatBitrate: function (bits) {
+            if (typeof bits !== 'number') {
+                return '';
+            }
+            if (bits >= 1000000000) {
+                return (bits / 1000000000).toFixed(2) + ' Gbit/s';
+            }
+            if (bits >= 1000000) {
+                return (bits / 1000000).toFixed(2) + ' Mbit/s';
+            }
+            if (bits >= 1000) {
+                return (bits / 1000).toFixed(2) + ' kbit/s';
+            }
+            return bits + ' bit/s';
+        },   
+        
+        _formatTime: function (seconds) {
+            var date = new Date(seconds * 1000),
+                days = parseInt(seconds / 86400, 10);
+            days = days ? days + 'd ' : '';
+            return days +
+                ('0' + date.getUTCHours()).slice(-2) + ':' +
+                ('0' + date.getUTCMinutes()).slice(-2) + ':' +
+                ('0' + date.getUTCSeconds()).slice(-2);
+        },     
+        
+        _formatPercentage: function (floatValue) {
+            return (floatValue * 100).toFixed(2) + ' %';
+        },        
+        
+        _renderExtendedProgress: function (data) {
+            return this._formatBitrate(data.bitrate) + ' | ' +
+                this._formatTime(
+                    (data.total - data.loaded) * 8 / data.bitrate
+                ) + ' | ' +
+                this._formatPercentage(
+                    data.loaded / data.total
+                ) + ' | ' +
+                this._formatFileSize(data.loaded) + ' / ' +
+                this._formatFileSize(data.total);
+        }        
     });
 
 }));
